72c0b3a1ad669c211c3159f3c5266f23c30bafdf
Nathan Buesgens <nathan@natb1.com>
2015-04-20 18:23:47 -0400

    boilerplate

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'git-deploy'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures git-deploy'
+long_description 'Installs/Configures git-deploy'
+version          '0.1.0'
